e7b6e122fe0671d67ad9a3a1004929550115a4c2
Pedro Barros <pedrokatsbarros@gmail.com>
2020-12-26 00:40:26 -0300

    Desenvolvida funcionalidade para definição de quantidade de notas e moedas que devem ser devolvidas como troco e acrescentado e modificado comentários

--- machine_software.s
+++ machine_software.s
@@ -10,7 +10,8 @@ virgula:   .asciiz ","
         .globl main
 main:
 		#Carregando valores iniciais nos registradores $s0 a $s7 e $t9 para fim de testes:
-		li $s0, 1
+		
+		li $s0, 3
 		li $s1, 1
 		li $s2, 1
 		li $s3, 1
@@ -21,7 +22,7 @@ main:
 		li $t9, 30
 		li $t3, 100
 		mult $t9, $t3
-		mflo $t9 #O $t9 representa o valor do produto selecionado x 100
+		mflo $t9 				#O $t9 representa o valor do produto selecionado x 100
 
 		
 		#Calculando valor em reais de notas:
@@ -76,20 +77,21 @@ main:
 		beq $t6, $0, exception	#Caso, o registrador $t6 tenha valor 0, significa que $t1 é menor do que $t9. Nesse caso, ocorrerá o desvio para a label 'exception' para tratamento dessa exceção
 
 result:
-		#Fluxo para apresentação da saída padrão
+		#Fluxo para apresentação da saída padrão:
 
-		li $t0, 0 #Carregando 0 no registrador $t0 para indicar que o valor inserido é suficiente para pagamento do produto selecionado
+		li $t0, 0 				#Carregando 0 no registrador $t0 para indicar que o valor inserido é suficiente para pagamento do produto selecionado
 		
-		#Calculando troco:
-		sub $t4, $t1, $t9
+		sub $t4, $t1, $t9		#Calculando troco:
 
 		#Convertendo o valor inserido para um valor com parte inteira e fracionária:
+
 		li $t3, 100
 		div $t1, $t3
 		mflo $t1
 		mfhi $t2
 
 		#Apresentando valor inserido:
+
 		li $v0, 4
 		la $a0, msg1
 		syscall
@@ -111,12 +113,14 @@ result:
 		syscall
 
 		#Convertendo o valor do produto selecionado para um valor com parte inteira e fracionária:
+
 		li $t3, 100
 		div $t9, $t3
 		mflo $t1
 		mfhi $t2
 
 		#Apresentando valor do produto selecionado:
+
 		li $v0, 4
 		la $a0, msg2
 		syscall
@@ -138,12 +142,14 @@ result:
 		syscall
 
 		#Convertendo o valor do troco para um valor com parte inteira e fracionária:
+
 		li $t3, 100
 		div $t4, $t3
 		mflo $t1
 		mfhi $t2
 
 		#Apresentando valor do troco:
+
 		li $v0, 4
 		la $a0, msg3
 		syscall
@@ -160,7 +166,123 @@ result:
 		move $a0, $t2
 		syscall
 
-		j exit
+initchange:
+		#Inicializando registradores que conterão a quantidade de notas e moedas que deverão ser dadas como troco:
+
+		li $s0, 0
+		li $s1, 0
+		li $s2, 0
+		li $s3, 0
+		li $s4, 0
+		li $s5, 0
+		li $s6, 0
+		li $s7, 0
+		li $t9, 0
+
+		#Inicializando registradores responsáveis por controlar os loops para calculo da qtd de notas e moedas do troco:
+
+		li $t3, 20 					#notas
+		li $t5, 50 					#moedas (tratadas como inteiros, sendo 50 equivalente a R$ 0,50)
+
+loopnotes:
+		#Fluxo responsável pelo cálculo de cada nota (e moeda de 1 real) que deverá ser devolvida como troco, considerando dar a menor quantidade de notas e moedas possíveis:
+
+		div $t1, $t3;				#Dividindo $t1 (parte inteira do troco) por $t3 (valor da nota atual) para ver quantas notas de determinado valor deverão ser dadas como troco
+		mflo $t4;
+		mult $t3, $t4
+		mflo $t7
+		sub $t1, $t1, $t7			#Subtraindo da parte inteira do troco, o valor que da nota atual que já será dado como troco			
+
+		
+		#Definindo qual será a próxima nota (ou moeda de 1 real) e em qual registrador a quantidade de notas, para troco, da nota atual deverá ser colocado:
+		
+		li $t7, 1
+		beq $t3, $t7, condloopnotes1 #Verificando se a nota atual ($t3) é igual a 1 e chamando label correspondente
+
+		li $t7, 2
+		beq $t3, $t7, condloopnotes2 #Verificando se a nota atual ($t3) é igual a 2 e chamando label correspondente
+
+		li $t7, 5
+		beq $t3, $t7, condloopnotes5 #Verificando se a nota atual ($t3) é igual a 5 e chamando label correspondente
+
+		li $t7, 10
+		beq $t3, $t7, condloopnotes10 #Verificando se a nota atual ($t3) é igual a 10 e chamando label correspondente
+
+		li $t7, 20
+		beq $t3, $t7, condloopnotes20 #Verificando se a nota atual ($t3) é igual a 20 e chamando label correspondente
+		
+		j loopcoins					  #Ao final desse loop, o fluxo saltará para o loop responsável pelo cálculos das qtds de cada moeda para troco
+
+condloopnotes1:
+		add $s4, $t4, $0				#Atribuindo a qtd de moedas de 1 real que deverão ser dadas como troco, ao registrador $s4
+		li $t3, 0						#Atribuindo próximo valor de nota que deverá ser testado. No caso, é zero para que se encerre o loopnotes
+		j loopnotes						#Saltando de volta para o loopnotes
+
+condloopnotes2:
+		add $s3, $t4, $0				#Atribuindo a qtd de notas de 2 reais que deverão ser dadas como troco, ao registrador $s3
+		li $t3, 1						#Atribuindo próximo valor de nota que deverá ser testado.
+		j loopnotes						#Saltando de volta para o loopnotes
+
+condloopnotes5:
+		add $s2, $t4, $0				#Atribuindo a qtd de notas de 5 reais que deverão ser dadas como troco, ao registrador $s2	
+		li $t3, 2						#Atribuindo próximo valor de nota que deverá ser testado.
+		j loopnotes						#Saltando de volta para o loopnotes
+
+condloopnotes10:
+		add $s1, $t4, $0				#Atribuindo a qtd de notas de 10 reais que deverão ser dadas como troco, ao registrador $s1
+		li $t3, 5						#Atribuindo próximo valor de nota que deverá ser testado.
+		j loopnotes						#Saltando de volta para o loopnotes
+
+condloopnotes20:
+		add $s0, $t4, $0				#Atribuindo a qtd de notas de 20 reais que deverão ser dadas como troco, ao registrador $s0
+		li $t3, 10						#Atribuindo próximo valor de nota que deverá ser testado.
+		j loopnotes						#Saltando de volta para o loopnotes
+
+loopcoins:
+
+		#Fluxo responsável pelo cálculo de cada moeda que deverá ser devolvida como troco, considerando dar a menor quantidade de moedas possível:
+
+		div $t2, $t5;				#Dividindo $t2 (parte fracionária do troco) por $t5 (valor da moeda atual) para ver quantas moedas de determinado valor deverão ser dadas como troco
+		mflo $t4;
+		mult $t5, $t4
+		mflo $t7
+		sub $t2, $t2, $t7			#Subtraindo da parte fracionária do troco, o valor que será dado como troco da moeda atual			
+
+		#Definindo qual será a próxima moeda (desconsiderando a moeda de 1 real, já que ela foi tratada no loop de notas) e em qual registrador a quantidade de moedas, para troco, da moeda atual deverá ser colocado:
+		
+		li $t7, 5
+		beq $t5, $t7, condloopcoins5 #Verificando se a moeda atual ($t5) é igual a 5 e chamando label correspondente
+
+		li $t7, 10
+		beq $t5, $t7, condloopcoins10 #Verificando se a moeda atual ($t5) é igual a 10 e chamando label correspondente
+
+		li $t7, 25
+		beq $t5, $t7, condloopcoins25 #Verificando se a moeda atual ($t5) é igual a 25 e chamando label correspondente
+
+		li $t7, 50
+		beq $t5, $t7, condloopcoins50 #Verificando se a moeda atual ($t5) é igual a 50 e chamando label correspondente
+	
+		j exit						  #Saindo do loop, saltando para a label 'exit', para encerramento da execução do programa
+
+condloopcoins5:
+		add $t9, $t4, $0				#Atribuindo a qtd de moedas de 5 centavos que deverão ser dadas como troco, ao registrador $t9
+		li $t5, 0						#Atribuindo próximo valor de moeda que deverá ser testado. No caso, é zero para que se encerre o loopcoins
+		j loopcoins						#Saltando de volta para o loopcoins
+
+condloopcoins10:
+		add $s7, $t4, $0				#Atribuindo a qtd de moedas de 10 centavos que deverão ser dadas como troco, ao registrador $s7
+		li $t5, 5						#Atribuindo próximo valor de moeda que deverá ser testado.
+		j loopcoins						#Saltando de volta para o loopcoins
+
+condloopcoins25:
+		add $s6, $t4, $0				#Atribuindo a qtd de moedas de 25 centavos que deverão ser dadas como troco, ao registrador $s6
+		li $t5, 10						#Atribuindo próximo valor de moeda que deverá ser testado.
+		j loopcoins						#Saltando de volta para o loopcoins
+
+condloopcoins50:
+		add $s5, $t4, $0				#Atribuindo a qtd de moedas de 50 centavos que deverão ser dadas como troco, ao registrador $s5
+		li $t5, 25						#Atribuindo próximo valor de moeda que deverá ser testado.
+		j loopcoins						#Saltando de volta para o loopcoins
 
 exception:
 		#Fluxo para tratamento da exceção de valor inserido menor do que valor do produto
@@ -172,3 +294,4 @@ exception:
 		syscall
 
 exit:
+	#Fluxo para encerramento do programa
